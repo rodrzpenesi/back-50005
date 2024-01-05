f3e38fd4a23f512707a84df598332adcdace4dff
Rodrigo Penesi <rodrz.penesi@gmail.com>
2024-01-05 03:08:59 -0300

    arreglo handlebars

--- src/handsOnLab/src/router/views.routes.js
+++ src/handsOnLab/src/router/views.routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import ProductManager from '../ProductManager.js'
 
+const productManager = new ProductManager('./src/handsOnLab/src/Products.json');
 const viewsRoutes = Router();
 
-viewsRoutes.get('/', (req, res) => {
-    res.render('index', {title: 'Probando'});
+viewsRoutes.get('/', async (req, res) => {
+    let allProducts = await productManager.getProducts();
+    res.render("home", {
+        title: "home",
+        products: allProducts
+    })
 });
 
 
